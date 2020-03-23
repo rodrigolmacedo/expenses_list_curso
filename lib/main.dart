b8305c751f87d172e8a0fece5164627d7f861d7c
Rodrigo Macedo <rodrigolima.18@hotmail.com>
2020-03-23 16:30:25 -0300

    aula 1

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text('Gráfico'),
+                color: Colors.blue,
+              ),
+            ),
+            Card(
+              child: Text('Lista de Transações'),
+            ),
+          ],
+        ));
+  }
+}
