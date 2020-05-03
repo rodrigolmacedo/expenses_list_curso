e26407e961223f8bbf99c8e1cfa3c5326d020b5b
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2020-05-03 17:23:05 -0300

    pequenos ajustes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,17 +6,16 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:expenses/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(ExpensesApp());
 
+/*
     // Verify that our counter starts at 0.
-   /* expect(find.text('0'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
